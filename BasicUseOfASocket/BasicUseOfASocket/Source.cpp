81c34fa0efade85a284ba9b65123a612ce8f15ce
DevoutChaos <chaosend50@gmail.com>
2015-08-26 13:15:53 -0700

    Updated to remove errors
    Added ws2_32 library
    Declared variables for send/recv

--- BasicUseOfASocket/BasicUseOfASocket/Source.cpp
+++ BasicUseOfASocket/BasicUseOfASocket/Source.cpp
@@ -1,29 +1,33 @@
 /*****
 Brief Implementation of FTP Client
 ******/
-//#define SOCK_STREAM
-#include <winsock.h>
+#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
 
 
 int main(int argc, char *argv[])
 {
 	int sock;
+	short count;
+	short len = 0;
+	char buf[1024];
+
 	struct sockaddr_in sockaddr;
 
 	memset(&sockaddr, 0, sizeof(sockaddr));
 	sock = socket(AF_INET, SOCK_STREAM, 0);
-	bzero(&sockaddr, sizeof(sockaddr));
+	//bzero(&sockaddr, sizeof(sockaddr));
 	sockaddr.sin_family = AF_INET;
 	sockaddr.sin_port = htons(21); //Host to Network Short
-	sockaddr.sin_addr.s_addr = inet_addr("..."); //IP
-	if (connect(sock, (struct sockaddr *)&sockaddr, sizeof(sockaddr)))
+	sockaddr.sin_addr.s_addr = inet_addr("134.139.48.252"); //IP
+	if (connect(sock, (struct sockaddr *)&sockaddr, sizeof(sockaddr)) == -1)
 	{
 		//Handle Error
 	}
 
-	byte_count = recv(sock, buf, len, 0);
+	count = recv(sock, buf, len, 0);
 
-	byte_count = send(sock, buf, len, 0);
+	count = send(sock, buf, len, 0);
 
 	return 0;
 }
