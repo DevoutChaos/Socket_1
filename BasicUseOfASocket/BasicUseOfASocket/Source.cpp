9d27bcce3e2028067da45f4b287fb90c4add803f
Liam Faulds <chaosend50@gmail.com>
2015-09-05 19:39:00 -0700

    IT LIVES!!!
    Moved WSAData from within the main method to join the includes/defines.
    Moved sections of declarations to allow WSAStartup to complete first.
    Updated cout to be more "user friendly"

--- BasicUseOfASocket/BasicUseOfASocket/Source.cpp
+++ BasicUseOfASocket/BasicUseOfASocket/Source.cpp
@@ -4,6 +4,7 @@
 #include <winsock2.h>
 #include <iostream>
 #include <errno.h>
+WSADATA wsaData;
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -14,14 +15,7 @@ int main(int argc, char *argv[])
 	short count;
 	short len = 0;
 	char buf[1024];
-
 	struct sockaddr_in sockaddr;
-	memset(&sockaddr, 0, sizeof(sockaddr));
-	sock = socket(AF_INET, SOCK_STREAM, 0); //Create socket. SOCK_STREAM is a type of socket, AF_INET is IPv4 protocols
-	sockaddr.sin_family = AF_INET; // AF_INET: address family that is used for the socket you're creating
-	sockaddr.sin_port = htons(21); //Host to Network Short
-	sockaddr.sin_addr.s_addr = inet_addr("130.179.16.34"); //IP of ftp.csulb.edu
-	WSADATA wsaData;
 
 	int err = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (err != 0) {
@@ -29,7 +23,14 @@ int main(int argc, char *argv[])
 		/* Winsock DLL.                                  */
 		printf("WSAStartup failed with error: %d\n", err);
 		return 1;
+
 	}
+
+	memset(&sockaddr, 0, sizeof(sockaddr));
+	sock = socket(AF_INET, SOCK_STREAM, 0); //Create socket. SOCK_STREAM is a type of socket, AF_INET is IPv4 protocols
+	sockaddr.sin_family = AF_INET; // AF_INET: address family that is used for the socket you're creating
+	sockaddr.sin_port = htons(21); //Host to Network Short
+	sockaddr.sin_addr.s_addr = inet_addr("130.179.16.34"); //IP of ftp.csulb.edu
 
 	if (sock == INVALID_SOCKET)
 	{
@@ -43,7 +44,7 @@ int main(int argc, char *argv[])
 
 	i = connect(sock, (struct sockaddr*) &sockaddr, sizeof(sockaddr));
 
-	cout << i << "\n";
+	//cout << i << "\n";
 
 	if (i == SOCKET_ERROR)
 	{
@@ -58,14 +59,14 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		cout << "Connected \n";
+		cout << "Please Wait. Establishing connection... \n";
 	}
-	
 	count = recv(sock, buf, len, 0);
-	cout << "Result/count = " << count;
-	count = send(sock, buf, len, 0);
 	
+	cout << "Connection complete. \n" << "Press any key to continue.";
 	cin.get();
 
+	count = send(sock, buf, len, 0);
+	
 	return 0;
 }
